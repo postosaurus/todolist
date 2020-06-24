a9a10dba9b85f89c588e5ae49d3ff0a1d0b33a1d
postosaurus <posty@riseup.net>
2020-06-24 23:21:36 +0200

    Version 2: it´s  a  todolist

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session
 from flask_session import Session
 
+import time
+
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
@@ -11,11 +13,30 @@ Session(app)
 def index():
     if session.get("notes") is None:
         session['notes']  = []
+    if session.get("dates") is None:
+        session['dates'] = []
     if  request.method == "POST":
         session["notes"].append(request.form.get("note"))
-    return render_template("index.html", notes=session["notes"])
+        t = time.ctime(time.time())
+        session["dates"].append(t)
 
-@app.route("/delete")
+    length = len(session["notes"])
+    return render_template("index.html", len=length, notes=session["notes"], date=session["dates"])
+
+@app.route("/delete", methods=["GET"])
 def delete():
-    session['notes']  = []
-    return render_template("index.html", notes=session["notes"], delete=True)
+    restart = False
+    delete = False
+    if request.method == "GET":
+        try:
+            index = int(request.args["id"])
+            del session['notes'][index]
+            del session['dates'][index]
+            delete = True
+        except  KeyError:
+            session['notes']  = []
+            session['dates'] =  []
+            restart = True
+
+    length = len(session["notes"])
+    return render_template("index.html", len=length, notes=session["notes"],  date=session["dates"], delete=delete,  restart=restart)
